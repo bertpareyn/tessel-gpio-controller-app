3241f98162d1385850d6b37dbd8e8b10d983b33e
Bert Pareyn <pareyn.bert@gmail.com>
2016-08-06 16:29:26 +0200

    Use environment variables for the Mongo connection configuration (when available)

--- config.js
+++ config.js
@@ -29,9 +29,9 @@ config.ui = {
 };
 
 config.db = {
-    'url': '',
-    'port': '',
-    'user': '',
-    'password': '',
-    'db': ''
+    'url': process.env.MONGO_URL || '',
+    'port': process.env.MONGO_PORT || '',
+    'user': process.env.MONGO_USER || '',
+    'password': process.env.MONGO_PASSWORD || '',
+    'db': process.env.MONGO_DB || ''
 };
